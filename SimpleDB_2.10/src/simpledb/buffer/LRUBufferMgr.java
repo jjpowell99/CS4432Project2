ad4805d73093dd86e234351257f2962ebcc8bf32
jjpowell <jjpowell@DESKTOP-PF1KURL.mshome.net>
2019-03-31 16:00:33 -0400

    Comments on LRUBufferMgr.

--- SimpleDB_2.10/src/simpledb/buffer/LRUBufferMgr.java
+++ SimpleDB_2.10/src/simpledb/buffer/LRUBufferMgr.java
@@ -13,11 +13,15 @@ import simpledb.file.*;
  *
  */
 class LRUBufferMgr extends AbsBufferMgr{
+	// CS4432-Project1: Adding hashmap to keep track of blocks existing in bufferpool and 
+	// queue for an LRU replacement policy of unpinned buffers.
+	
    private Buffer[] bufferpool;
-   private HashMap<Block, Buffer> existingBlockMap; // 4432
+   private HashMap<Block, Buffer> existingBlockMap; 
    private int numAvailable;
    private Queue<Buffer> possibleBuffers;
-   /**
+   /** CS4432-Project1: Added initialization of an empty hashmap and queue filled with the empty buffers.
+    * 
     * Creates a buffer manager having the specified number 
     * of buffer slots.
     * This constructor depends on both the {@link FileMgr} and
@@ -51,7 +55,9 @@ class LRUBufferMgr extends AbsBufferMgr{
          buff.flush();
    }
    
-   /**
+   /** CS4432-Project1: Added a put statement to the hashmap to keep track of the buffer 
+    * for blk, now that it is pinned in the buffer pool for efficient access thereafter.
+    * 
     * Pins a buffer to the specified block. 
     * If there is already a buffer assigned to that block
     * then that buffer is used;  
@@ -94,7 +100,8 @@ class LRUBufferMgr extends AbsBufferMgr{
       return buff;
    }
    
-   /**
+   /** CS4432-Project1: Adds this buffer to the end of the queue now that it is unpinned, and thus, available to be replaced.
+    * 
     * Unpins the specified buffer.
     * @param buff the buffer to be unpinned
     */
@@ -113,14 +120,19 @@ class LRUBufferMgr extends AbsBufferMgr{
    int available() {
       return numAvailable;
    }
-   
+   /** CS4432-Project1: Changed the scan for an existing buffer to using the hashmap to find it efficiently if it exists.
+    * Additionally, the queue is updated so that this most recently accessed block would exist at the end if unpinned.
+    * Finds buffer that this block exists in if it does exist and returns it. 
+    * @param blk Block to find in bufferpool if it exists.
+    * @return Buffer in bufferpool that block exists in. Null is returned if the block does not exist in the buffer pool.
+    */
    private Buffer findExistingBuffer(Block blk) {
 	   Buffer bufferToAccess = existingBlockMap.get(blk);
 	   if(bufferToAccess != null) {
 		   possibleBuffers.remove(bufferToAccess);
 		   if(bufferToAccess.isPinned())
 			   possibleBuffers.add(bufferToAccess);
-		   return bufferToAccess; // 4432
+		   return bufferToAccess; 
 	   }
       /*for (Buffer buff : bufferpool) {
          Block b = buff.block();
@@ -129,13 +141,17 @@ class LRUBufferMgr extends AbsBufferMgr{
       }*/
       return null;
    }
-   
+   /** CS4432-Project1: Changed the scan for an available buffer to instead use an LRU replacement policy.
+    * Finds the least recently used unpinned buffer and returns it.
+    * @return Available unpinned buffer that was the least recently used, or null if all buffers are pinned
+    */
    private Buffer chooseUnpinnedBuffer() {
-	   while(possibleBuffers.size() > 0) { // CS 4432 LRU, remove
-		   if(possibleBuffers.peek().isPinned()) {
+	   // CS4432-Project1: While there are potential available buffers, find the first valid one and return
+	   while(possibleBuffers.size() > 0) { 
+		   if(possibleBuffers.peek().isPinned()) { // CS4432-Project1: If pinned, shouldn't be in queue, so remove it.
 			   possibleBuffers.poll();
 		   }
-		   else {
+		   else { // CS4432-Project1: If unpinned buffer return the buffer and update the hashmap if it has a block currently.
 			   Buffer toRemove = possibleBuffers.poll();
 			   if(toRemove.block() != null) {
 				   existingBlockMap.remove(toRemove.block());
@@ -149,6 +165,9 @@ class LRUBufferMgr extends AbsBufferMgr{
          return buff;*/
       return null;
    }
+   /** CS4432-Project1: Changed the toString by overwriting it to list the toStrings for each buffer in the bufferpool
+    * @return String of toStrings of buffers in bufferpool with header describing buffer manager type.
+    */
    public String toString() {
 	   String output = "LRU Buffer Manager\n";
 	   for(Buffer b: bufferpool) {
